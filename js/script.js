ac3da294095bc9fc8115427ab464b3aad7aebe4b
Martin Cervantes <cervantes.martine@gmail.com>
2020-07-28 16:40:14 -0600

    Add book to the array

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let myLibrary = [];
+
+function Book(author, title, numPages, beenRead) {
+  this.author = author;
+  this.title = title;
+  this.numPages = numPages;
+  this.beenRead = beenRead;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+
+}
+
+myLibrary = [new Book('Julio Verne', 'Five weeks in a balloon', '288', true)];
+addBookToLibrary(new Book('Julio Verne', 'Journey to the center of the earth', '368', false));
+addBookToLibrary(new Book('Julio Verne', 'Around the world in 80 days', '400', false));
