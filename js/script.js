d31b824cd67a506c3df1d61ff466c78c76c821a6
Martin Cervantes <cervantes.martine@gmail.com>
2020-07-28 17:19:23 -0600

    Add function to render information to the html

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ let myLibrary = [];
 
 const form = document.getElementById('form_container');
 
+const books = document.getElementById('books_container');
+
 function Book(author, title, numPages, beenRead) {
   this.author = author;
   this.title = title;
@@ -39,16 +41,40 @@ function showForm() {
   }
 }
 
+function render() {
+  let innerHTML = '';
+
+  for (let i = 0; i < myLibrary.length; i += 1) {
+    innerHTML += `<div class='book'><p><span>Author</span> ${myLibrary[i].author}</p>`;
+    innerHTML += `<i class='fas fa-trash-alt' onclick="removeBookToLibrary(${i})"></i>`;
+    innerHTML += `<p><span>Title</span> ${myLibrary[i].title}</p>`;
+    innerHTML += `<p><span># pages</span> ${myLibrary[i].numPages}</p>`;
+    innerHTML += "<p><span>Have you read this book?</span><input type='checkbox' ";
+
+    if (myLibrary[i].beenRead) {
+      innerHTML +=  'checked ';
+    }
+
+    innerHTML += `onclick="toggleBeenRead(${i})" /></p></div>`;
+  }
+
+  books.innerHTML = innerHTML;
+}
+
 function addBookToLibrary(book) {
   myLibrary.push(book);
 
   hideForm();
+
+  render();
 }
 
 function removeBookToLibrary(index) {
   if (confirm('Would you like to delete this book ??')) {
     myLibrary.splice(index, 1);
   }
+
+  render();
 }
 
 myLibrary = [new Book('Julio Verne', 'Five weeks in a balloon', '288', true)];
